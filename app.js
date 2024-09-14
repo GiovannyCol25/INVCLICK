13ddd93bc414bbeb4477c134817fe9dab502cf00
Giovanny <capera.giovanny25@gmail.com>
2024-09-13 20:40:49 -0500

    carga de archivos del proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var errorMsg = document.getElementById('error-msg');
+
+    // Limpiar el mensaje de error previo
+    errorMsg.textContent = '';
+
+    // Para propósitos de demostración, asumimos que el usuario y la contraseña correctos son "user" y "password"
+    if(username === "user" && password === "password") {
+        // Guardar el nombre de usuario en el almacenamiento local
+        localStorage.setItem('username', username);
+        // Redirigir a la página de bienvenida
+        window.location.href = 'main.html';
+    } else {
+        errorMsg.textContent = 'Invalid username or password';
+        errorMsg.style.color = 'red';
+    }
+});
